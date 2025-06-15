ba3cf371902236c39179cec7ff53233dfcbbe31c
= <=>
2025-06-15 21:45:27 +0300

    update for neovide

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,47 +1,60 @@
-
 function GetRelativeFilePath()
-
   local file_path = vim.fn.expand('%:p')
-
   local relative_path = vim.fn.fnamemodify(file_path, ":~")
-
   return relative_path
 end
 
--- Логируем начало настройки плагина lualine
-local navic = require("nvim-navic")
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons",
-  "LuaDist/dkjson",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "LuaDist/dkjson",
+    "SmiteshP/nvim-navic",  -- Рекомендую использовать официальный репозиторий navic
   },
   config = function()
+    -- Настройка шрифтов (требует поддержки терминалом и Nerd Font)
+    vim.g.neovide_font = "0xProto Nerd Font Mono:h12"		
+    
+    local navic = require("nvim-navic")
     require("lualine").setup({
       options = {
-        section_separators = "",
-        component_separators = "",
+        theme = 'auto',  -- Автоматически подстраивается под colorscheme
+        component_separators = { left = '│', right = '│' },  -- Тонкие разделители
+        section_separators = { left = '', right = '' },  -- Пустые разделители секций
+        disabled_filetypes = {  -- Отключение для определенных типов файлов
+          'NvimTree',
+          'packer',
+          'alpha',
+          'dashboard'
+        },
+        globalstatus = true,  -- Глобальный статусбар для Neovim 0.7+
       },
       sections = {
-        lualine_a = {"mode"},
-        lualine_b = {"branch", "diff"},
-        lualine_c = {'filename'},
-        lualine_y = {'progress'},
+        lualine_a = {
+          { 'mode', icon = '', padding = { left = 1, right = 1 } },  -- Иконка режима
+        },
+        lualine_b = {
+          { 'branch', icon = '', padding = { left = 1, right = 1 } },  -- Иконка ветки
+          { 'diff', padding = { left = 1, right = 1 }, symbols = { added = ' ', modified = ' ', removed = ' ' } },
+          { 'diagnostics', padding = { left = 1, right = 1 } },
+        },
+        lualine_x = {
+          { 'encoding', padding = { left = 1, right = 1 } },
+          { 'fileformat', padding = { left = 1, right = 1 }, symbols = { unix = '', dos = '', mac = '' } },
+          { 'filetype', padding = { left = 1, right = 1 } },
+        },
+        lualine_y = {
+          { 'progress', padding = { left = 1, right = 1 } },
+        },
         lualine_z = {
-	 {
-                "navic",
-    
-                -- Component specific options
-                color_correction = nil, -- Can be nil, "static" or "dynamic". This option is useful only when you have highlights enabled.
-                                        -- Many colorschemes don't define same backgroud for nvim-navic as their lualine statusline backgroud.
-                                        -- Setting it to "static" will perform a adjustment once when the component is being setup. This should
-                                        --   be enough when the lualine section isn't changing colors based on the mode.
-                                        -- Setting it to "dynamic" will keep updating the highlights according to the current modes colors for
-                                        --   the current section.
-    
-                navic_opts = nil  -- lua table with same format as setup's option. All options except "lsp" options take effect when set here.
-            }
+          { 'location', padding = { left = 1, right = 1 } },
         }
       },
+      inactive_sections = {
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+      },
+      extensions = { 'fugitive', 'nvim-tree', 'toggleterm' }  -- Поддержка расширений
     })
   end,
 }
